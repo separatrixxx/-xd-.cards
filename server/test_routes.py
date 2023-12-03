0f03f469668c8e09dbc4107c13d1e041cadee164
AntonCkya <sinyukovanton@yandex.ru>
2023-12-03 14:38:21 +0300

    added text on image

--- server/test_routes.py
+++ server/test_routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException
 from fastapi.responses import FileResponse
 import response_models
+from pynter.pynter import generate_captioned
 
 router = APIRouter(
     prefix="/test",
@@ -19,4 +20,7 @@ router = APIRouter(
 async def get_card(params: str):
     if params == None or params == "":
         raise HTTPException(status_code=400, detail="Empty request")
-    return FileResponse("test_image.png")
+    im = generate_captioned(params, image_path="./test_image.png", size=(512, 512),
+                        font_path="./font.ttf", filter_color=(0, 0, 0, 20))
+    im.convert("RGB").save("new_test_image.png")
+    return FileResponse("new_test_image.png")
