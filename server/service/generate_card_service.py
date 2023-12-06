bad440c6f9f0d26278f3c6a504eef2965f72427a
Денис Устинов <denisustinov2003@mail.ru>
2023-12-06 13:45:47 +0300

    Добавил исключение на отсутствие токена

--- server/service/generate_card_service.py
+++ server/service/generate_card_service.py
@@ -12,9 +12,11 @@ class GenerateCardService:
     def generate_card(self, params_for_image : request_models.QueryParams):
         if params_for_image.text_stable_diffusion == None or params_for_image.text_stable_diffusion == "":
             raise ValueError("No text for Stable Diffusion text was defined")
-        image_bytes = self.__stable_diffusion_service.get_stable_diffusion_image(params_for_image.text_stable_diffusion)
-        if (b'loading' in image_bytes.getvalue()):
-            raise ValueError("Stable Diffusion needs some time (around 10-13 minutes) to cool down") 
+        image_bytes = ""
+        try:
+            image_bytes = self.__stable_diffusion_service.get_stable_diffusion_image(params_for_image.text_stable_diffusion)
+        except Exception as e:
+            raise ValueError(e)
         if (params_for_image.text_image != None and params_for_image.text_image != ""):
             im = generate_captioned(params_for_image.text_image, image_path=image_bytes, size=(512, 512),
                                 font_path=self.__font_path, filter_color=(0, 0, 0, 20))
--- server/service/stable_diffusion_service.py
+++ server/service/stable_diffusion_service.py
@@ -6,12 +6,18 @@ import json
 class StableDiffusionService:
     
     def __init__(self):
-         self.__stable_diffusion_url = "https://api.wizmodel.com/sdapi/v1/txt2img"
-         self.__api_key = open('stable_diffusion_token.txt').read()
-         self.__headers = {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + self.__api_key
-            }
+        self.__stable_diffusion_url = "https://api.wizmodel.com/sdapi/v1/txt2img"
+        self.__api_key = ""
+        try:
+            self.__api_key = open('stable_diffusion_token.txt').read()
+        except:
+            raise ValueError("No token for Stable Diffusion was provided in stable_diffusion_token.txt file or file is missing or corrupted")
+        if (self.__api_key == ""):
+            raise ValueError("No token for Stable Diffusion was provided in stable_diffusion_token.txt")
+        self.__headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + self.__api_key
+        }
         
     def get_stable_diffusion_image(self, text: str):
         payload = json.dumps({
@@ -22,4 +28,6 @@ class StableDiffusionService:
         base64_string = response.json()['images'][0]
         image_data = base64.b64decode(base64_string)
         image_bytes = BytesIO(image_data)
+        if (b'loading' in image_bytes.getvalue()):
+            raise ValueError("Stable Diffusion needs some time (around 10-13 minutes) to cool down") 
         return image_bytes
